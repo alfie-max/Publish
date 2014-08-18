52a00c5dfaddcad9b484a77b84fb639b33da0bd2
alfie <alfie.2012@gmail.com>
2014-08-19 01:42:31 +0530

    Uses tempfile to read messages

--- publish.py
+++ publish.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 
-import argparse
+import os
 import sys
+import argparse
+import tempfile
+import subprocess
+
+from configobj import ConfigObj
+from validate import Validator
 
 class ThrowingArgumentParser(argparse.ArgumentParser):
     """ Overrides argparse error function and
     Handles conditions like invalid arguments"""
     def error(self, message):
         self.print_help()
+        sys.exit(1)
 
-def parse_args():
+def Parse_Args():
     """ Parse arguments to the app """
     parser = ThrowingArgumentParser()
     parser.add_argument(
@@ -19,10 +26,38 @@ def parse_args():
 
     if len(sys.argv)==1:
         parser.print_help()
+        sys.exit(1)
 
     return parser.parse_args()
 
-args = parse_args()
+def Add_Field(field, fieldType, cfgFile, cfgSpec):
+    config = ConfigObj(cfgFile)
+    config[field] = ''
+    config.write()
+
+    spec = ConfigObj(cfgSpec)
+    spec[field] = fieldType
+    spec.write()
+
+def Validate(cfgFile, cfgSpec):
+    config = ConfigObj(cfgFile, configspec = cfgSpec)
+    validator = Validator()
+    result = config.validate(validator)
+    return result
+
+
+""" Here program starts execution """
+args = Parse_Args()
+(fn, cfgFile) = tempfile.mkstemp()
+(fn, cfgSpec) = tempfile.mkstemp()
 
 if args.twitter:
     print "Its twitter time :)"
+
+Add_Field('Message', 'string', cfgFile, cfgSpec)
+subprocess.call('%s %s' % (os.getenv('EDITOR'), cfgFile), shell = True)
+
+
+if Validate(cfgFile, cfgSpec) != True:
+    print "Input file validation failed"
+    sys.exit(1)
--- tests/ui_test.py
+++ tests/ui_test.py
@@ -1,8 +1,14 @@
+import os
 import argparse
+import tempfile
+import subprocess
+from configobj import ConfigObj
+from validate import Validator
 
 class ThrowingArgumentParser(argparse.ArgumentParser):
     def error(self, message):
         self.print_help()
+        #sys.exit(1)  --> Ignored due to test fail
 
 parser = ThrowingArgumentParser()
 
@@ -11,13 +17,43 @@ parser.add_argument(
     action = 'store_true',
     help = 'Send to twitter')
 
+def Add_Field(field, fieldType, cfgFile, cfgSpec):
+    config = ConfigObj(cfgFile)
+    config[field] = ''
+    config.write()
+
+    spec = ConfigObj(cfgSpec)
+    spec[field] = fieldType
+    spec.write()
+
+def Validate(cfgFile, cfgSpec):
+    config = ConfigObj(cfgFile, configspec = cfgSpec)
+    validator = Validator()
+    result = config.validate(validator)
+    return result
+
 def pass_args(args):
     if len(args)==0:
         parser.print_help()
+        #sys.exit(1)  --> Ignored due to test fail
     args = parser.parse_args(args.split())
+    
     if args.twitter:
         print "Its twitter time :)"
 
-pass_args('-f')
+    (fn, cfgFile) = tempfile.mkstemp()
+    (fn, cfgSpec) = tempfile.mkstemp()
+
+    Add_Field('Message', 'string', cfgFile, cfgSpec)
+    subprocess.call('%s %s' % (os.getenv('EDITOR'), cfgFile), shell = True)
+
+
+    if Validate(cfgFile, cfgSpec) != True:
+        print "Input file validation failed"
+        #sys.exit(1)  --> Ignored due to test fail
+
+
+
 pass_args('-t')
+pass_args('-f')
 pass_args('')
