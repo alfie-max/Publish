ef61d2ab064b15f7cc03cfca082b6247ff3702e8
alfie <alfie.2012@gmail.com>
2014-09-03 01:48:32 +0530

    Twitter api tests added

--- tests/twitter_test.py
+++ tests/twitter_test.py
@@ -1,6 +1,30 @@
-from ..plugins.twitter import Twitter
+from ..plugins import twitter
+import mock_tweepy
+import mock_browser
+twitter.tweepy = mock_tweepy
+twitter.raw_input = mock_tweepy.raw_input
+twitter.webbrowser = mock_browser
 
-T = Twitter()
+T = twitter.Twitter()
 
 def test_get_auth_info():
     T.GetAuthInfo()
+
+def test_VerifyCredentials():
+    T.VerifyCredentials()
+
+def test_Authorize():
+    T.Authorize()
+
+def test_verifyFields():
+    T.VerifyFields({'Message':'Test Message'})
+
+def test_verifyFields_failed():
+    T.VerifyFields({'Message':''})
+
+def test_SendMsg():
+    T.SendMsg({'Message':'Test Message'})
+
+def test_SendMsg_as_img():
+    T.SendMsg({'Message':"""This is a long message to test sending as image.
+    Make this long and long and long..... yes its really long"""})
