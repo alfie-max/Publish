cda5e79165aa24ebe2b58103558b7874a2fe6c64
Shahul Hameed <beingshahul@gmail.com>
2014-09-14 12:46:37 +0530

    Added getpath method

--- plugins/fb.py
+++ plugins/fb.py
@@ -93,7 +93,38 @@ class Facebook(Channel):
             else:
                 endpoint = "http://" + ENDPOINT
             return endpoint + path + '?' + urllib.urlencode(args)
+	def get(path, args):
+            return urllib2.urlopen(get_url(path, args=args)).read()
+        
+        ''' Steps to authenticate '''
+        ui_print (colored('Authorizing Facebook Account...', 'yellow'))
+        auth_url = get_url('/oauth/authorize',
+                           {'client_id' : APP_ID,
+                            'redirect_uri' : REDIRECT_URI,
+                            'scope' : 'publish_actions'})
+        savout = os.dup(1)
+        os.close(1)
+        os.open(os.devnull, os.O_RDWR)
+        try:
+            webbrowser.open(auth_url)
+        finally:
+            os.dup2(savout, 1)
 
+        httpd = BaseHTTPServer.HTTPServer(('127.0.0.1', 8080), RequestHandler)
+        while ACCESS_TOKEN is None:
+            httpd.handle_request()
+
+        ''' Update Config file with Token Keys '''
+        cfg = ConfigParser.RawConfigParser()
+        cfg.read('.publish')
+        if not cfg.has_section('Facebook'):
+            cfg.add_section('Facebook')
+        cfg.set('Facebook', 'Access Token', hexlify(ACCESS_TOKEN))
+        with open('.publish', 'wb') as configfile:
+            cfg.write(configfile)
+
+        if not self.VerifyCredentials():
+            raise AuthorizationError('Authorization Failed')
 
     def VerifyFields(self, fields):
         Message = Facebook['Message']
