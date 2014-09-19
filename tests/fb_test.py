461931e1a9f12086c1f710333bcd83e8057528ec
alfie <alfie.2012@gmail.com>
2014-09-19 21:05:37 +0530

    Tests for facebook plugin

---
+++ tests/fb_test.py
@@ -0,0 +1,42 @@
+import pytest
+from ..plugins import fb
+import mock_facebook
+import mock_webbrowser
+
+fb.facebook = mock_facebook
+fb.webbrowser = mock_webbrowser
+
+F = fb.Facebook()
+
+def test_get_auth_info():
+    F.GetAuthInfo()
+
+def test_VerifyCredentials():
+    F.VerifyCredentials()
+
+def test_reset():
+    F.Reset()
+
+def test_verifyFields():
+    F.VerifyFields({'Message':'Test Message'})
+
+def test_verifyFields_failed():
+    F.VerifyFields({'Message':''})
+
+def test_SendMsg():
+    F.SendMsg({'Message' : 'None'})
+
+def test_SendMsg_fail():
+    F.SendMsg({'Message' : 'error'})
+
+def test_SendMsg_fail2():
+    try:
+        F.SendMsg({'Message' : 'neterror'})
+    except Exception:
+        pass
+
+def test_Authorize():
+    try:
+        F.Authorize()
+    except SystemExit:
+        pass
---
+++ tests/mock_webbrowser.py
@@ -0,0 +1,4 @@
+import sys
+
+def open(url):
+    sys.exit()
